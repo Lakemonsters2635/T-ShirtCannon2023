cb69ee5ca7803c5b958795135281392363c6114c
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2023-10-11 20:52:08 -0700

    Initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public final class Constants {
+    public static final int LEFT_JOYSTICK_CHANNEL = 0;
+    public static final int RIGHT_JOYSTICK_CHANNEL = 1;
+    
+    public static final int ROTARY_CHANNEL = 1; // Channel for rotating motor
+
+    public static final double ROTARY_SPEED = 0.25; // Speed of the rotation 
+
+    
+}
